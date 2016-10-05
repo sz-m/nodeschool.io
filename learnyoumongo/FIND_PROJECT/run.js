5b57cbb4bf560b338ec4214720e8c6f89e210591
_michal <sz-m@users.noreply.github.com>
2016-10-05 22:51:50 +0200

    FIND PROJECT

---
+++ learnyoumongo/FIND_PROJECT/run.js
@@ -0,0 +1,29 @@
+var mongo = require('mongodb').MongoClient;
+
+var age = +process.argv[2];
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+
+mongo.connect(url, function(err, db) {
+  if(err)
+    throw err;
+
+  var parrots = db.collection('parrots');
+
+  parrots.find(
+    {
+      age: {$gt: age}
+    }, {
+      name: 1,
+      age: 1,
+      _id: 0
+    }
+  ).toArray(function(err, documents) {
+    if(err)
+      throw err;
+
+    console.log(documents);
+
+    db.close();
+  });
+});
